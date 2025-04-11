781fbeb08443f3bda46dbeb13666addfd56d41ec
Lan Yang <layang@c2r4p3.42perpignan.fr>
2025-04-11 19:26:10 +0200

    leak checks 0411

---
+++ Minishell_0411_leak_check/src/export.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: layang <layang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 17:26:37 by nimorel           #+#    #+#             */
+/*   Updated: 2025/04/11 17:34:11 by layang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	*ft_extract_name(char *str)
+{
+	char	*name;
+	int		i;
+
+	i = 0;
+	while (str[i] && str[i] != '=')
+		i++;
+	name = malloc(i + 1);
+	if (!name)
+		return (NULL);
+	ft_strlcpy(name, str, i + 1);
+	return (name);
+}
+
+int	ft_update_var(t_env *env, char *name, char *value)
+{
+	t_env	*current;
+
+	current = env;
+	while (current)
+	{
+		if (ft_strcmp(current->name, name) == 0)
+		{
+			if (value)
+			{
+				free(current->value);
+				current->value = value;
+			}
+			free(name);
+			free(value);
+			return (SUCCESS);
+		}
+		current = current->next;
+	}
+	//return (printf("update env: update %s failed\n", name), FAILURE);
+	return (FAILURE);
+}
+
+static int	ft_add_var(t_env **env, char *name, char *value)
+{
+	t_env	*new_node;
+	t_env	*current;
+
+	if (!value)
+		value = ft_strdup("");
+	new_node = ft_create_env_node(name, value);
+	if (!new_node)
+	{
+		perror("Failed to create new env node");
+		free(value);
+		free(name);
+		return (FAILURE);
+	}
+	if (*env)
+	{
+		current = *env;
+		while (current->next)
+			current = current->next;
+		current->next = new_node;
+	}
+	else
+		*env = new_node;
+	free(name);
+	free(value);
+	return (SUCCESS);
+}
+
+int	ft_export(t_token *tokens, t_env **env)
+{
+	char	*name;
+	char	*value;
+
+	if (!tokens->next)
+	{
+		ft_env(*env);
+		return (SUCCESS);
+	}
+	tokens = tokens->next;
+	name = ft_extract_name(tokens->value);
+	if (!name)
+		return (FAILURE);
+	value = ft_strchr(tokens->value, '=');
+	if (value)
+		value = ft_strdup(value + 1);
+	if (ft_update_var(*env, name, value) == SUCCESS)
+		return (SUCCESS);
+	return (ft_add_var(env, name, value));
+}
